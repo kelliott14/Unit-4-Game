b85cdc1431ed5974497b2b35db6b36784454a437
Kelliott <52241744+kelliott14@users.noreply.github.com>
2019-08-22 15:14:32 +1000

    all working. Just making finishing touches to design

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
+    
 //Scorecard variables
 var wins = 0;
 var losses = 0;
-var scorecardText = document.getElementById("ScorecardText");
+var guessNumber = 0;
 var yourNumber = 0;
 var orangeStone = 0;
 var redStone = 0;
@@ -12,86 +13,67 @@ var greenStone = 0;
 var yellowStone = 0;
 
 
-//Scorecard reset
-function scorecardreset(){
-    wins = 0;
-    losses = 0;
-}
+//Scorecard text at the start of the game
+$("#ScorecardText").text("Click on a stone to start");
+
 
 //Start Round. Resets target number and the stones's numbers.
 function startRound(){
-    var guessNumber = Math.floor(Math.random() * (120 - 19)) + 19;
+    guessNumber = Math.floor(Math.random() * (120 - 19)) + 19;
     $(".GetThisText").text("Your target:  " + guessNumber);
     
     yourNumber = 0
-    $(".YourNumText").text(yourNumber + " : Your number so far");
+    $(".YourNumText").text(yourNumber + " : Your number");
 
     orangeStone = Math.floor(Math.random() * (12 - 1)) + 1;
+    $("#orangeStone").val(orangeStone);
+
     redStone = Math.floor(Math.random() * (12 - 1)) + 1;
+    $("#redStone").val(redStone);
+
     blueStone = Math.floor(Math.random() * (12 - 1)) + 1;
+    $("#blueStone").val(blueStone);
+
     purpleStone = Math.floor(Math.random() * (12 - 1)) + 1;
+    $("#purpleStone").val(purpleStone);
+
     greenStone = Math.floor(Math.random() * (12 - 1)) + 1;
+    $("#greenStone").val(greenStone);
+
     yellowStone = Math.floor(Math.random() * (12 - 1)) + 1;
+    $("#yellowStone").val(yellowStone);
 }
 
 //Start the game
-    startRound();
-    console.log("o" + orangeStone)
-    console.log("r" + redStone)
-    console.log("b" + blueStone)
-    console.log("g" + greenStone)
-    console.log("p" + purpleStone)
-    console.log("y" + yellowStone)
+startRound();
 
-//On click Orange
-    $("#orangeStone").on("click", function() {
-        yourNumber = yourNumber + orangeStone
-        $(".YourNumText").text(yourNumber + " : Your number so far");
 
+//On each click, the stone's value is added to the yourNumber total
+$(".stone").on("click", function(){
+    var clickednumber = $(this).val();
+    yourNumber = parseInt(yourNumber);
+    clickednumber = parseInt(clickednumber);
+
+    yourNumber = yourNumber + clickednumber;
+    $(".YourNumText").text(yourNumber + " : Your number");
+
+    //if else statement for winning or losing
+    if (yourNumber == guessNumber){
+        wins++
+        startRound();
+        $("#ScorecardText").text("You won!!");
+    
+    }else if(yourNumber > guessNumber){
+        losses++
+        startRound();
+        $("#ScorecardText").text("You lost, try again!");
+    }
+    
+    //Scoreboard display
+    $("#WinsText").text("Wins: " + wins);
+    $("#LossesText").text("Losses: " + losses);
+    
     })
-
-//On click Red
-$("#redStone").on("click", function() {
-    yourNumber = yourNumber + redStone
-    $(".YourNumText").text(yourNumber + " : Your number so far");
-
-})
-
-//On click Blue
-$("#blueStone").on("click", function() {
-    yourNumber = yourNumber + blueStone
-    $(".YourNumText").text(yourNumber + " : Your number so far");
-
-})
-
-//On click Green
-$("#greenStone").on("click", function() {
-    yourNumber = yourNumber + greenStone
-    $(".YourNumText").text(yourNumber + " : Your number so far");
-
-})
-
-//On click purple
-$("#purpleStone").on("click", function() {
-yourNumber = yourNumber + purpleStone
-$(".YourNumText").text(yourNumber + " : Your number so far");
-
-})
-
-//On click yellow
-$("#yellowStone").on("click", function() {
-yourNumber = yourNumber + yellowStone
-$(".YourNumText").text(yourNumber + " : Your number so far");
-
-})
-
-
-
-
-
-
-
-
 
 
 
